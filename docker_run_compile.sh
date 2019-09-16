84d25d6567424cb6487c6a0178d60f5ab0b50e93
Francois WELLENREITER <francois.wellenreiter@atos.net>
2019-09-16 11:14:28 +0200

    initial version

---
+++ docker_run_compile.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+DOCKER=${DOCKER:-"/usr/bin/docker"}
+
+TOOLS=`dirname $0`
+
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    --rm \
+    --privileged
+    --ipc=host \
+    -v $HOME/work:/work \
+    -v $HOME:/home \
+    -v $TOOLS:/code \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --user=`id -u`:`id -g` \
+    -e USER=$USER \
+    "
+
+if [ a$MEM != a ]
+then
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    -m $MEM \
+    "
+fi
+
+if [ a$CPUSET != a ]
+then
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    --cpuset-cpus=$CPUSET \
+    "
+fi
+
+if [ a$PROCS != a ]
+then
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    -e PROCS=$PROCS \
+    "
+fi
+
+if [ a$PARALL != a ]
+then
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    -e PARALL=$PARALL \
+    "
+fi
+
+if [ a$DONT_COMPILE != a ]
+then
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    -e DONT_COMPILE=$DONT_COMPILE \
+    "
+fi
+if [ a$DO_CLEAN != a ]
+then
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    -e DO_CLEAN=$DO_CLEAN \
+    "
+fi
+
+if [ a$DONT_GATHER != a ]
+then
+CMDLINE_DOCKER_RUN="$CMDLINE_DOCKER_RUN \
+    -e DONT_GATHER=$DONT_GATHER \
+    "
+fi
+
+if [ a$DO_LOG = a -o a$DO_LOG = a0 ]
+then
+exec $DOCKER run -it $CMDLINE_DOCKER_RUN \
+    $@
+else
+exec $DOCKER run -t $CMDLINE_DOCKER_RUN \
+    $@ \
+    | tee $HOME/docker_run_`date '+%Y-%m-%d_%H-%M-%S'`.log
+fi
