83ed4e31a758ce4330806ff4bbc1e21cd1a8dccc
Francois WELLENREITER <francois.wellenreiter@atos.net>
2020-01-30 14:15:33 +0100

    use -- as separator for docker_run_compile

--- docker_run_compile.py
+++ docker_run_compile.py
@@ -8,20 +8,18 @@ import os, sys
 
 IMAGE="compile:latest-libc"
 
-def parent(args, params):
+def parent(args):
     logging.info("Running image : {}".format(args.image))
     logging.debug("{} run -it --rm --privileged"
         " -v /var/run/docker.sock:/var/run/docker.sock"
         " -v {}:/src"
         " -v {}:/code"
         " -v /tmp/root:/root"
-        " {} python3 /code/compile.py {} {} {}"
+        " {} python3 /code/compile.py {}"
         .format(args.docker, os.getcwd(),
         os.path.join(os.path.dirname(os.path.abspath(__file__))),
         args.image,
-        " -l " + str(args.loglevel),
-        " -v" if args.verbose else "",
-        ' '.join(map(str, params))))
+        ' '.join(map(str, args.params[1:]))))
     try:
         os.system(args.docker + " run -it --rm --privileged"
           " -v /var/run/docker.sock:/var/run/docker.sock"
@@ -32,7 +30,7 @@ def parent(args, params):
           " python3 /code/compile.py" +
           " -l " + str(args.loglevel) +
           (" -v" if args.verbose else "") +
-          " " + ' '.join(map(str, params)))
+          " " + ' '.join(map(str, args.params[1:])))
     except Exception as err:
         logging.error("calling {} failed, err {}", args.docker, err)
 
@@ -42,18 +40,19 @@ def parse():
     parser.add_argument("-d", "--docker", action = "store", type = str, default = "/usr/bin/docker")
     parser.add_argument("-i", "--image", action = "store", type = str, default = IMAGE)
     parser.add_argument("-l", "--loglevel", action = "store", type = int, default = logging.ERROR)
-    args, params = parser.parse_known_args()
+    parser.add_argument("params", nargs=argparse.REMAINDER)
+    args = parser.parse_args()
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=args.loglevel)
 
-    return args, params
+    return args
 
 def main():
-    args, params = parse()
-    parent(args, params)
+    args = parse()
+    parent(args)
 
 
 if __name__ == "__main__":
