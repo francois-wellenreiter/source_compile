1bb70e4928e0ef5b44bdee38761d192ed5bbee75
Francois WELLENREITER <wellen@free.fr>
2020-05-19 13:13:46 +0200

    execute as current user

--- docker_run_compile.py
+++ docker_run_compile.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
 
-import os, sys
+import os, sys, pwd
 import argparse
 import logging
 import docker
 from docker.types import LogConfig
 
-IMAGE="compile:latest-libc"
-BASE_CMD=[ "python3", "/code/compile.py" ]
-SRC="/src"
-CODE="/code"
-ROOT="/root"
-ROOT_VOLUME="compile_root"
-DOCK_SOCK="/var/run/docker.sock"
-FORMAT = '%(asctime)-15s - %(message)s'
-NAME = "compile" + os.getcwd().replace('/', '_')
 
 def parent(args):
     logging.warning("Running image : {}".format(args.image))
+
+    DOCK_SOCK="/var/run/docker.sock"
+    SRC="/src"
+    CODE="/code"
+    HOME="/home/" + pwd.getpwuid(os.getuid()).pw_name
+    USER=pwd.getpwuid(os.getuid()).pw_name
+    NAME = "compile" + os.getcwd().replace('/', '_')
 
     cli = docker.APIClient()
     cont = cli.create_container(image = args.image,
@@ -25,22 +23,29 @@ def parent(args):
             *args.command,
             *args.params[1:]
         ],
-        name = NAME,
+        name = "compile" + os.getcwd().replace('/', '_'),
+        environment = {
+            "HOME" : HOME,
+            "USER" : USER,
+            "USERNAME" : USER,
+            "LOGNAME" : USER
+        },
+        user = str(os.getuid()) + ":" + str(os.getgid()),
         working_dir = SRC,
-        volumes = [ ROOT, SRC, CODE, DOCK_SOCK ],
+        volumes = [ SRC, CODE, HOME, DOCK_SOCK ],
         host_config = cli.create_host_config(
           auto_remove = True,
           binds={
-            ROOT_VOLUME: {
-              'bind': ROOT,
-              'mode': 'rw',
-            },
             os.getcwd(): {
               'bind': SRC,
               'mode': 'rw',
             },
             os.path.join(os.path.dirname(os.path.abspath(__file__))): {
               'bind': CODE,
+              'mode': 'rw',
+            },
+            HOME: {
+              'bind': HOME,
               'mode': 'rw',
             },
             DOCK_SOCK: {
@@ -63,12 +68,13 @@ stderr = True, follow = True, timestamps = False, tail = "all"):
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action = "store_true")
-    parser.add_argument("-i", "--image", action = "store", type = str, default = IMAGE)
-    parser.add_argument("-c", "--command", action = "store", type = str, nargs='+', default = BASE_CMD)
+    parser.add_argument("-i", "--image", action = "store", type = str, default = "compile:latest-libc")
+    parser.add_argument("-c", "--command", action = "store", type = str, nargs='+', default = [ "python3", "/code/compile.py" ])
     parser.add_argument("-l", "--loglevel", action = "store", type = int, default = logging.WARNING)
     parser.add_argument("params", nargs = argparse.REMAINDER)
     args = parser.parse_args()
 
+    FORMAT='%(asctime)-15s - %(message)s'
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG, format = FORMAT)
     else:
