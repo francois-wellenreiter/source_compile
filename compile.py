28517fa15b40459876f6e18171b00f25c1eecc66
Francois WELLENREITER <wellen@free.fr>
2020-06-04 19:43:49 +0200

    manage compilation dependencies through networkx graph

--- compile.py
+++ compile.py
@@ -40,93 +40,93 @@ MVNOPTS="__MVN_OPTS__"
 
 def progress(op_code, cur_count, max_count=None, message='', fn = None, key = ""):
     if fn is None:
-        logging.debug( '{} - Downloading : ( {} / {} ) {}\r'.format(key, cur_count, max_count, message))
+        logging.debug( 'Downloading -\t{} : ( {} / {} ) {}\r'.format(key, cur_count, max_count, message))
     else:
-        logging.debug( '{} - {} : ( {} / {} ) {}\r'.format(key, fn.__name__, cur_count, max_count, message))
+        logging.debug( 'Downloading -\t{} : {} - ( {} / {} ) {}\r'.format(key, fn.__name__, cur_count, max_count, message))
 
 
 
 def clone(k, v, args):
-    logging.info("{} - Cloning".format(k))
+    logging.info("Cloning -\t{}".format(k))
     try:
         repo = Repo.clone_from(v[URL], os.path.join(args.directory, k),
             branch = v[BRANCH], progress = partial(progress, fn = Repo.clone_from, key = k))
     except Exception as err:
-        logging.error("{} - Error when cloning {}".format(k, err))
+        logging.error("Error when cloning {} with {}".format(k, err))
         return
     try:
         repo.submodule_update(recursive = True, init = True)
     except Exception as err:
-        logging.debug("{} - No submodules to update {}".format(k, err))
-    logging.info("{} - Cloned".format(k))
+        logging.debug("No submodules {} to update {}".format(k, err))
+    logging.info("Cloned -\t{}".format(k))
 
 
 
 def update(k, v, args):
-    logging.info("{} - Updating".format(k))
+    logging.info("Updating -\t{}".format(k))
     try:
         repo = Repo(os.path.join(args.directory, k))
         origin = repo.remotes[ORIGIN]
         origin.fetch(progress = partial(progress, fn = origin.fetch, key = k))
         repo.heads.master.set_tracking_branch(origin.refs.master)
     except Exception as err:
-        logging.error("{} - Error no repository found {}".format(k, err))
+        logging.error("Error no repository {} found {}".format(k, err))
         return
     if args.clean:
         try:
             repo.head.reset(index=True, working_tree=True)
         except Exception as err:
-            logging.error("{} - Error when resetting {}".format(k, err))
+            logging.error("Error when resetting {} {}".format(k, err))
 
         try:
             repo.git.gc()
         except Exception as err:
-            logging.error("{} - Error when compressing git repository {}".format(k, err))
+            logging.error("Error when compressing git repository {} with {}".format(k, err))
     try:
         repo.remotes.origin.pull(progress = partial(progress, fn = repo.remotes.origin.pull, key = k))
     except Exception as err:
-        logging.error("{} - Error when pulling {}".format(k, err))
+        logging.error("Error when pulling {} with {}".format(k, err))
     try:
         repo.submodule_update(recursive = True, init = True)
     except Exception as err:
-        logging.debug("{} - No submodules to update {}".format(k, err))
+        logging.debug("No submodules to update {} with {}".format(k, err))
     logging.info("{} - Updated".format(k))
 
 
 def clean(k, v, args):
-    logging.info("{} - Cleaning".format(k))
+    logging.info("Cleaning -\t{}".format(k))
     os.chdir(os.path.join(args.directory, k))
     if CLEAN in v:
         for cmd in v[CLEAN]:
-            logging.debug("{} - Executing {}".format(k, cmd))
+            logging.debug("Executing -\t {} - {}".format(k, cmd))
             os.system(cmd)
-    logging.info("{} - Cleaned".format(k))
+    logging.info("Cleaned -\t{}".format(k))
 
 
 def build(k, v, args):
-    logging.info("{} - Building".format(k))
+    logging.info("Building -\t{}".format(k))
     os.chdir(os.path.join(args.directory, k))
     if BUILD in v:
         for cmd in v[BUILD]:
-            logging.debug("{} - Executing {}".format(k, cmd))
+            logging.debug("Executing -\t {} - {}".format(k, cmd))
             os.system(cmd)
-    logging.info("{} - Built".format(k))
+    logging.info("Built -\t{}".format(k))
 
 
 def worker(data, args):
-    for k, v in data.items():
-        if args.target is None or k in args.target:
-            if v[ENABLED] or args.force:
-                logging.info("{} - Managing".format(k))
-                if not os.path.exists(os.path.join(args.directory, k)):
-                    clone(k, v ,args)
-                if args.update:
-                    update(k, v, args)
-                if args.clean:
-                    clean(k, v, args)
-                if args.build:
-                    build(k, v, args)
-                logging.info("{} - Managed".format(k))
+    k, v = data
+    if args.target is None or k in args.target:
+        if v[ENABLED] or args.force:
+            logging.info("Managing -\t{}".format(k))
+            if not os.path.exists(os.path.join(args.directory, k)):
+                clone(k, v ,args)
+            if args.update:
+                update(k, v, args)
+            if args.clean:
+                clean(k, v, args)
+            if args.build:
+                build(k, v, args)
+            logging.info("Managed -\t{}".format(k))
 
 
 #######################################
@@ -145,14 +145,23 @@ class Deps():
                     self.graph.add_node(dep)
                     self.graph.add_edge(k, dep)
 
+        if not nx.is_directed_acyclic_graph(self.graph):
+            raise TypeError
+
     def items(self):
         return self.graph.nodes(data = True)
 
-    def __iter__(self):
-        return self.graph.__iter__()
+    def is_full(self):
+        return self.graph.number_of_nodes() > 0
 
-    def next(self):
-        return self.graph.next()
+    def tearoff_leaves(self):
+        leaves = []
+        for k, v in self.graph.nodes(data = True):
+            if self.graph.out_degree(k) == 0:
+                leaves += [ (k, v) ]
+        for k, _ in leaves:
+            self.graph.remove_node(k)
+        return leaves
 
 
 def load_files(dir):
@@ -163,7 +172,7 @@ def load_files(dir):
                 with open(os.path.join(dirpath, f), 'r') as h:
                     try:
                         deps.parse(yaml.safe_load(h))
-                    except yaml.YAMLError as exc:
+                    except (TypeError, yaml.YAMLError) as exc:
                         logging.info("Error {} while loading {}".format(exc, f))
     return deps
 
@@ -172,10 +181,8 @@ def print_stats(deps):
     for k, v in sorted(deps.items()):
         if ENABLED in v and v[ENABLED]:
             print("+--->\t{}".format(k))
-            cmd = subprocess.Popen(["/usr/bin/cloc", "--json", k],
-                stdout = subprocess.PIPE)
-
-            out,_ = cmd.communicate()
+            out, _ = subprocess.Popen(["/usr/bin/cloc", "--json", k],
+                stdout = subprocess.PIPE).communicate()
             d = json.loads(out)
 
             for k_, v_ in sorted(d.items()):
@@ -198,8 +205,9 @@ def parent(args):
     elif args.stats:
         print_stats(deps)
     else:
-        with mp.Pool(processes = args.parallelize) as pool:
-            res = pool.map(partial(worker, args = args), deps)
+        while deps.is_full():
+            with mp.Pool(processes = args.parallelize) as pool:
+                res = pool.map(partial(worker, args = args), deps.tearoff_leaves())
 
 
 def parse():
